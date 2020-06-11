88791453094eb6d9a3f344739f0c71aaa7810b9a
Tako <takoforme@gmail.com>
2020-06-11 19:30:02 +0900

    ログイン、ログアウトの実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
-  def create
+  def create #ログイン認証の実装
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
+    end
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
