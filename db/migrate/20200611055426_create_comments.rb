6fc5afa6ddabb49e0544dd1d628384b281542724
Tako <takoforme@gmail.com>
2020-06-11 14:57:26 +0900

    コメントマイグレーションファイルを修正 & bundle isntall実行

--- db/migrate/20200611055426_create_comments.rb
+++ db/migrate/20200611055426_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.references :board, null: false, foreign_key: true
-      t.string :name
-      t.text :comment
+      t.references :board, null: false, foreign_key: true #references :xxで、xxに紐づける事ができる
+      t.string :name, null: false #空打ちはできないように設定
+      t.text :comment, null: false
 
       t.timestamps
     end
