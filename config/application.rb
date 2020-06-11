1017f39d49fb796791113e44859767691d5a6df9
Tako <takoforme@gmail.com>
2020-06-11 13:26:43 +0900

    indexにページネーションを実装

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module BoardApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
   end
 end
